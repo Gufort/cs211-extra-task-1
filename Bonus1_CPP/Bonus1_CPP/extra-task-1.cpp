9bea45cac8416eb121747921bcd09ae173d2eea4
Игорь Аведисьян <avedisian@sfedu.ru>
2024-10-01 14:42:00 +0300

    changes in asserts

--- Bonus1_CPP/Bonus1_CPP/extra-task-1.cpp
+++ Bonus1_CPP/Bonus1_CPP/extra-task-1.cpp
@@ -64,31 +64,5 @@ double time_from_utc(int utc_offset, double time){
     if (res < 0) res += 24.0;
     else if (res >= 24) res -= 24.0;
     return res;
-    /*
-        return utc time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
 }
+

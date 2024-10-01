e56cd490c414a1326016b983f87c8e7f2ca764e7
Игорь Аведисьян <avedisian@sfedu.ru>
2024-10-01 13:19:31 +0300

    realization of hours_difference and add asserts

--- Bonus1_CPP/Bonus1_CPP/extra-task-1.h
+++ Bonus1_CPP/Bonus1_CPP/extra-task-1.h
@@ -1,6 +1,15 @@
 #ifndef EXTRA_TASK
 #define EXTRA_TASK
 
+
+//Return the number of seconds later that a time in seconds
+//time_2 is than a time in seconds time_1.
 double seconds_difference(double, double);
+
+//return the number of hours later that a time in seconds
+//time_2 is than a time in seconds time_1
+double hours_difference(double, double);
+
+
 
 #endif // !EXTRA_TASK
